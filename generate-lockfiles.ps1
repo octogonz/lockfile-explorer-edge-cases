4ebe4533ccf331183750b06c52a9a11723686ccd
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-09-25 22:36:40 -0400

    Rename files

--- generate-lockfiles.ps1
+++ generate-lockfiles.ps1
@@ -8,25 +8,25 @@ pnpm install
 cd ..
 
 Write-Host "Deleting old lockfiles..."
-del pnpm-lock.yaml, pnpm-lock-5.4.yaml, pnpm-lock-6.0.yaml, pnpm-lock-9.0.yaml -Force -ErrorAction SilentlyContinue
+del pnpm-lock.yaml, pnpm-lock-v5.4.yaml, pnpm-lock-v6.0.yaml, pnpm-lock-v9.0.yaml -Force -ErrorAction SilentlyContinue
 
 Write-Host "Deleting node_modules..."
 rd node_modules -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "`n==> Generating pnpm-lock-5.4.yaml"
 & "pnpm-cli/pnpm7/node_modules/.bin/pnpm" install
-move -Force pnpm-lock.yaml pnpm-lock-5.4.yaml
+move -Force pnpm-lock.yaml pnpm-lock-v5.4.yaml
 
 Write-Host "Deleting node_modules..."
 rd node_modules -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "`n==> Generating pnpm-lock-9.0.yaml"
 & "pnpm-cli/pnpm10/node_modules/.bin/pnpm" install
-move -Force pnpm-lock.yaml pnpm-lock-9.0.yaml
+move -Force pnpm-lock.yaml pnpm-lock-v9.0.yaml
 
 Write-Host "Deleting node_modules..."
 rd node_modules -Recurse -Force -ErrorAction SilentlyContinue
 
 Write-Host "`n==> Generating pnpm-lock-6.0.yaml"
 & "pnpm-cli/pnpm8/node_modules/.bin/pnpm" install
-move -Force pnpm-lock.yaml pnpm-lock-6.0.yaml
+move -Force pnpm-lock.yaml pnpm-lock-v6.0.yaml
